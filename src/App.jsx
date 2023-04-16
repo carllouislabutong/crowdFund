2a631304d528cf2fdc13864acacc5a8679f91209
Carl <carllouislabutong@gmail.com>
2023-04-16 19:42:34 +0800

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,10 @@ function App() {
           </ul>
           <div className="burger-menu" onClick={handleMenu}>
             {menu ? (
-              <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
+              <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
                 <g fill="#FFF" fill-rule="evenodd">
-                  <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z" />
+                  <path d="M2.404.782l11.314 11.314-2.122 2.122L.282 2.904z" />
+                  <path d="M.282 12.096L11.596.782l2.122 2.122L2.404 14.218z" />
                 </g>
               </svg>
             ) : (
