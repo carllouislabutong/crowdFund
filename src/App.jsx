634843dbe88ea5852933d0abd7abcf7c4e23431e
Carl <carllouislabutong@gmail.com>
2023-04-16 19:39:22 +0800

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -43,10 +43,9 @@ function App() {
                 </g>
               </svg>
             ) : (
-              <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+              <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
                 <g fill="#FFF" fill-rule="evenodd">
-                  <path d="M2.404.782l11.314 11.314-2.122 2.122L.282 2.904z" />
-                  <path d="M.282 12.096L11.596.782l2.122 2.122L2.404 14.218z" />
+                  <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z" />
                 </g>
               </svg>
             )}
